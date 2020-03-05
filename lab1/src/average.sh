f105dda6f07a1dafbfc59cb2845a514bab98cdf0
redheat2020kate <redheat2020kate@gmail.com>
2020-03-05 14:25:43 +0000

    Add arg2.txt, average.sh, new.sh, rand.sh, text.txt

---
+++ lab1/src/average.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+k=$#
+avg=0
+sum=0
+for i in $@
+do
+sum=$(($sum+$i))
+done
+echo avg=$(($sum/$#))
---
+++ lab1/src/new.sh
@@ -0,0 +1,8 @@
+chmod ugo+x hello.sh
+#! /bin/sh
+echo “Текущий путь: ”
+pwd
+echo “Текущая дата и время: ”
+date
+echo «Содержание переменной окружения PATH: »
+echo $PATH
